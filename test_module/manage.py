425d5260737ea3b636807d670629b32cd17e17a8
Seiya Asahina <seiya@asahina>
2020-12-10 17:59:26 +0900

    ver 1.0

---
+++ test_module/manage.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#introディレクトリのviews.pyとmodels.pyをimport
+from intro import views,models
+
+print("私はmanage.pyです。私を実行して他のPythonファイル内の処理を呼び出します。")
+views.views_intro()
+models.models_intro()
+
+"""
+モジュールに関しては下記を参照
+https://note.nkmk.me/python-relative-import/
+
+"""
+
